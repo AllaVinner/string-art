f3354e6423da58822ed2c3dba53589f195106251
allavinner <joe.w.ottosson@gmail.com>
2023-07-23 19:14:35 +0200

    Added algorithm to crawl through pixels in ribon

--- scrathc.py
+++ scrathc.py
@@ -66,4 +66,65 @@ fig.update_layout(
 fig.show()
 
 
+# Line image creator 
+mean_image_intensity = 10.
+
+x1 = 245
+y1 = 23
+
+x2 = 23,
+y2 = 23
+
+ribon_radius = 4.
+picture_radius = 50
+picture_center = (50, 60)
+
+def is_in_picture_circle(x, y, radius, center_x, center_y):
+    return (x-center_x)**2+(y-center_y)**2 < radius **2
+
+def is_in_ribon(x, y, radius, pixel_1, pixel_2):
+    v = pixel_2-pixel_1
+    a = np.array([x,y])
+    w = x-(pixel_1+ np.inner(a-pixel_1, v)/np.inner(v,v)*v)
+    return np.inner(w,w) < radius **2
+
+def get_neighbours(x,y):
+    return [[x+1, y],
+            [x, y+1]
+            [x-1, y]
+            [x, y-1]]
+
+pixels_to_visit = [[x1, y1]]
+approved_pixels = set()
+while len(pixels_to_visit) > 0:
+    x, y = pixels_to_visit.pop()
+    approved_pixels.add((x, y))
+    for nx, ny in get_neighbours(x, y):  
+        if not is_in_picture_circle(nx,ny, picture_radius, picture_center[0], picture_center[1]):
+            continue
+        if not is_in_ribon(nx,ny, picture_radius, picture_center[0], picture_center[1]):
+            continue
+        if (nx. ny) in approved_pixels:
+            continue
+        pixels_to_visit.append((nx. ny))
+
+
+a = set()
+v = np.array([1,2,3])
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
